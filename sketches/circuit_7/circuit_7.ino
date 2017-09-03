f8e3c1b6eb17bed5024e2618231201d26231e63a
rgarro <rgarro@gmail.com>
2017-09-02 18:13:36 -0600

    [Delivers #150281950] push apagale la luz ...

---
+++ sketches/circuit_7/circuit_7.ino
@@ -0,0 +1,57 @@
+//la guila demphra esta conmigo desde fines de los 90 ...
+
+const int temperaturePin = A0;
+
+
+void setup()
+{
+
+  Serial.begin(9600); //Initialize serial port & set baud rate to 9600 bits per second (bps)
+}
+
+
+void loop()
+{
+
+
+  float voltage, degreesC, degreesF; //Declare 3 floating point variables
+
+  voltage = getVoltage(temperaturePin); //Measure the voltage at the analog pin
+
+  degreesC = (voltage - 0.5) * 100.0; // Convert the voltage to degrees Celsius
+
+  degreesF = degreesC * (9.0 / 5.0) + 32.0; //Convert degrees Celsius to Fahrenheit
+  
+  //Now print to the Serial monitor. Remember the baud must be 9600 on your monitor!
+  // These statements will print lines of data like this:
+  // "voltage: 0.73 deg C: 22.75 deg F: 72.96"
+  
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.print("  deg C: ");
+  Serial.print(degreesC);
+  Serial.print("  deg F: ");
+  Serial.println(degreesF);
+
+  delay(1000); // repeat once per second (change as you wish!)
+}
+
+
+float getVoltage(int pin)   //Function to read and return
+              //floating-point value (true voltage)
+              //on analog pin 
+{
+
+  return (analogRead(pin) * 0.004882814); 
+  // This equation converts the 0 to 1023 value that analogRead()
+  // returns, into a 0.0 to 5.0 value that is the true voltage
+  // being read at that pin.
+}
+
+// Other things to try with this code:
+
+//   Turn on an LED if the temperature is above or below a value.
+
+//   Read that threshold value from a potentiometer - now you've
+//   created a thermostat!
+
