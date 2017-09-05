bd7501706ce5a2213366ac8f1b67db941ef775a1
rgarro <rgarro@gmail.com>
2017-09-05 12:00:42 -0600

    [Delivers #150281968] Sara Conor lider de la resistencia ...

---
+++ sketches/circuit_8/circuit_8.ino
@@ -0,0 +1,63 @@
+
+/*
+yo no soy siervo del rey ...
+*/
+
+
+#include <Servo.h>  // servo library
+
+
+Servo servo1;  // servo control object
+
+
+void setup()
+{
+
+  servo1.attach(9, 1200, 2100);  //Connect the servo to pin 9
+                //with a minimum pulse width of
+                //900 and a maximum pulse width of
+                //2100. 
+
+}
+
+
+void loop()
+{
+  int position;
+  
+  // To control a servo, you give it the angle you'd like it
+  // to turn to. Servos cannot turn a full 360 degrees, but you
+  // can tell it to move anywhere between 0 and 180 degrees.
+
+  // Change position at full speed:
+
+  servo1.write(90);    // Tell servo to go to 90 degrees
+
+  delay(1000);         // Pause to get it time to move
+
+  servo1.write(180);   // Tell servo to go to 180 degrees
+
+  delay(1000);         // Pause to get it time to move
+
+  servo1.write(0);     // Tell servo to go to 0 degrees
+
+  delay(1000);         // Pause to get it time to move
+  
+ 
+  // Tell servo to go to 180 degrees, stepping by two degrees each step
+ 
+  for(position = 0; position < 180; position += 2)
+  {
+    servo1.write(position);  // Move to next position
+    delay(20);               // Short pause to allow it to move
+  }
+
+  // Tell servo to go to 0 degrees, stepping by one degree each step
+
+  for(position = 180; position >= 0; position -= 1)
+  {                                
+    servo1.write(position);  // Move to next position
+    delay(20);               // Short pause to allow it to move
+  }
+}
+
